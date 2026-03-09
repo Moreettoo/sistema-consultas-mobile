1afd92a16e7df0cfbf86824fc6401b8c051935fa
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 20:34:43 -0300

    second commit

--- App.tsx
+++ App.tsx
@@ -1,30 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+type Consulta = {
+  id: number;
+  paciente: string;
+  medico: string;
+  data: string;
+  status: "agendada" | "confirmada" | "cancelada" | "realizada";
+};
 export default function App() {
+  const [consulta, setConsulta] = useState<Consulta>({
+    id: 1,
+    paciente: "Carlos Andrade",
+    medico: "Dr. Roberto Silva",
+    data: "28/02/2026",
+    status: "agendada",
+  });
+  function confirmarConsulta() {
+    setConsulta({
+      ...consulta,
+      status: "confirmada",
+    });
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>2CCR</Text>
-      <Text style={styles.subtitulo}>Curso de React Native</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Sistema de Consultas</Text>
+      <View style={styles.card}>
+        <Text>Paciente: {consulta.paciente}</Text>
+        <Text>Médico: {consulta.medico}</Text>
+        <Text>Data: {consulta.data}</Text>
+        <Text>Status: {consulta.status}</Text>
+        {consulta.status === "agendada" && (
+          <View style={styles.botao}>
+          <Button title="Confirmar Consulta" onPress={confirmarConsulta} />
+          </View>
+        )}
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#dff1df',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#d8d8d8",
+    alignItems: "center",
+    justifyContent: "center",
   },
   titulo: {
-    color: '#056b138d',
-    fontSize: 20,
-    fontWeight: 'bold'
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
   },
-  subtitulo: {
-    color: '#025f1e',
-    fontSize: 16
+  card: {
+    width: "80%",
+    padding: 20,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  botao: {
+    marginTop: 60,
   }
 });
+ 
